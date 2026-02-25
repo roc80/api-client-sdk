37f0d3593064a15de863ba94fccc9af98b57726f
lipeng <lipeng@fire369.com>
2026-02-25 11:58:14 +0800

    fix: 适配spring-boot 4.x版本

--- src/main/java/com/roc/apiclientsdk/client/ApiClient.java
+++ src/main/java/com/roc/apiclientsdk/client/ApiClient.java
@@ -1,23 +1,24 @@
 package com.roc.apiclientsdk.client;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.roc.apiclientsdk.module.User;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
-import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author lipeng
  * @since 2026/2/24 17:38
  */
 @AllArgsConstructor
-@Slf4j
 public class ApiClient {
     private String accessKey;
     private String secretKey;
@@ -26,21 +27,54 @@ public class ApiClient {
      * todo 根据url来调用接口
      */
     public String getName(User user) {
+        String bodyJson = JSONUtil.toJsonStr(user);
         HttpResponse httpResponse = HttpRequest.post("http://localhost:8010/api/name")
-                .addHeaders(getHeaderMap())
-                .body(JSONUtil.toJsonStr(user))
+                .addHeaders(getHeaderMap(bodyJson))
+                .body(bodyJson)
                 .execute();
         String result = httpResponse.body();
-        log.info("ApiClient#getName result status :{}", httpResponse.getStatus());
-        log.info("ApiClient#getName result:{}", result);
+        System.out.println("ApiClient#getName result status : " +  httpResponse.getStatus());
+        System.out.println("ApiClient#getName result: " + result);
         return result;
     }
 
-    private Map<String, String> getHeaderMap() {
+    /**
+     *
+     * @param bodyJson 请求体JSON字符串
+     * @return 请求头Map
+     */
+    private Map<String, String> getHeaderMap(String bodyJson) {
         HashMap<String, String> headerMap = new HashMap<>(5);
         headerMap.put("Content-Type", "application/json");
         // ak nonce timestamp sign->sha512(ak+nonce+timestamp+sk)
+        headerMap.put("access-key", accessKey);
+        String nonce = RandomUtil.randomNumbers(5);
+        headerMap.put("nonce", nonce);
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        headerMap.put("timestamp", timestamp);
+        byte[] gennedSign = genSign(DigestAlgorithm.SHA512, accessKey, nonce, timestamp, secretKey, bodyJson);
+        String sign = Arrays.toString(gennedSign);
+        headerMap.put("sign", sign);
         return headerMap;
+    }
+
+    /**
+     * @param digestAlgorithm 摘要算法
+     * @param accessKey       用户身份凭证
+     * @param nonce           随机数，防重放
+     * @param timestamp       时间戳，和随机数配合使用，防重放
+     * @param secretKey       用户身份密钥
+     * @param bodyJson        请求体JSON字符串
+     * @return 对于上述防篡改部分的SHA-512哈希值
+     */
+    private byte[] genSign(DigestAlgorithm digestAlgorithm,
+                           String accessKey,
+                           String nonce,
+                           String timestamp,
+                           String secretKey,
+                           String bodyJson) {
+        String data = accessKey + nonce + timestamp + secretKey + bodyJson;
+        return DigestUtil.digester(digestAlgorithm).digest(data);
     }
 
 }
--- src/main/java/com/roc/apiclientsdk/config/ClientSdkConfig.java
+++ src/main/java/com/roc/apiclientsdk/config/ClientSdkConfig.java
@@ -1,28 +1,32 @@
 package com.roc.apiclientsdk.config;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import com.roc.apiclientsdk.client.ApiClient;
 
 import lombok.Data;
 
 /**
+ * 从 application.yml 读取 api.client 配置，自动创建 Bean
+ *
  * @author lipeng
  * @since 2026/2/24 16:45
  */
-@Configuration
-@ConfigurationProperties("api.client")
-@ComponentScan
+@AutoConfiguration
+@EnableConfigurationProperties(ClientSdkConfig.class)
 @Data
+@ConfigurationProperties("api.client")
 public class ClientSdkConfig {
 
     private String accessKey;
     private String secretKey;
 
     @Bean
+    @ConditionalOnMissingBean
     public ApiClient apiClient() {
         return new ApiClient(accessKey, secretKey);
     }
